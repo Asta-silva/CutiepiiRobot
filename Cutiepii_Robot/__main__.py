e479ddc1e4cace90d7c2df3fadde41da0be09936
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2021-12-31 15:13:27 +0530

    Update __main__.py

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -279,7 +279,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_animation(
-            GROUP_START_IMG, caption= "<b>Yes, Darling I'm alive!\nHaven't sleep since</b>: <code>{}</code>".format(
+            GROUP_START_IMG, caption= "<b>OHH! Yes, Darling!I won't sleep until I satisfy you!\n<b>Haven't slept since\nHaven't sleep since</b>: <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
