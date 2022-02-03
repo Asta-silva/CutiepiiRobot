83891763c5c080ea0a2e1c88ade684c2ffb125a5
Nor_baka <98837331+Baka-me@users.noreply.github.com>
2022-02-03 09:50:25 +0530

    Update userinfo.py

--- Cutiepii_Robot/modules/userinfo.py
+++ Cutiepii_Robot/modules/userinfo.py
@@ -109,7 +109,7 @@ def hpmanager(user):
         if not sql.get_user_bio(user.id):
             new_hp -= no_by_per(total_hp, 10)
 
-        if is_afk(user.id):
+        if is_user_afk(user.id):
             afkst = set_afk(user.id)
             # if user is afk and no reason then decrease 7%
             # else if reason exist decrease 5%
