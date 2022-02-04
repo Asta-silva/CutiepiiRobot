b376fa4117b7ef4a677c15731522cc9b471e5397
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-04 22:16:28 +0530

    Update userinfo.py

--- Cutiepii_Robot/modules/userinfo.py
+++ Cutiepii_Robot/modules/userinfo.py
@@ -469,7 +469,7 @@ def stats(update, context):
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
             + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/Freia_Updates)\n\n"
-            + "╘══「 Powered By [Komi - San](https://t.me/KomiXryu_Bot) | [Network](https://t.me/toman_network) 」\n",
+            + "╘══「 Made By [ᴀsᴛᴀ•°•](https://t.me/Asta_silva02) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
