41da2a79bf23e1913e52eb499ff7f8378659c496
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-13 10:32:12 +0530

    Update stickers.py

--- Cutiepii_Robot/modules/stickers.py
+++ Cutiepii_Robot/modules/stickers.py
@@ -13,8 +13,8 @@ from telegram import TelegramError, Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from SiestaRobot import dispatcher
-from SiestaRobot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot import dispatcher
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
