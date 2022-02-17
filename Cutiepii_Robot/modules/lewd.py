192bb9e3954592469443eef4611f6ad057e2eb35
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-17 17:06:30 +0530

    Update lewd.py

--- Cutiepii_Robot/modules/lewd.py
+++ Cutiepii_Robot/modules/lewd.py
@@ -9,17 +9,15 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from Cutiepii_Robot.events import register
-from Cutiepii_Robot import telethn as tbot
 from time import sleep
 from PIL import Image
 
-from Cutiepii_Robot import BOT_USERNAME
 from telegram import Bot, Chat, Message, MessageEntity, ParseMode, Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CommandHandler, run_async, CallbackContext
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
+from Cutiepii_Robot import BOT_USERNAME
 from Cutiepii_Robot.events import register
 from Cutiepii_Robot import telethn as tbot
 from Cutiepii_Robot import dispatcher, updater
